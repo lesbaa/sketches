e8103c536040f4c0a1af0c2ffb14be1578917b9a
Les Moffat <les.moffat@stv.tv>
2018-07-11 13:22:45 +0100

    pixelate!

---
+++ app/shaders/pixelate/index.js
@@ -0,0 +1,24 @@
+import fragment from './frag.glsl'
+
+export default {
+  fragment,
+  uniforms: {
+    uTime: {
+      type: 'f',
+      value: 0,
+    },
+    uMouse: {
+      type: 'v2',
+      value: [0, 0],
+    },
+    uViewportResolution: {
+      type: 'v2',
+      value: [window.innerWidth, window.innerHeight],
+    },
+    uTransitionProgress: {
+      type: 'f',
+      value: 0,
+    },
+    
+  },
+}
